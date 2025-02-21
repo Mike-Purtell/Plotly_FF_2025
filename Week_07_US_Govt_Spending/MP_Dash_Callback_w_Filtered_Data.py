8c040936eb6eda1802b2e19cf436a28a298e6dd6
Michael Purtell <mpurtell1024@gmail.com>
2025-02-21 13:29:48 -0800

    final week 7 with callback usecase for even or odd value

--- Week_07_US_Govt_Spending/MP_Dash_Callback_w_Filtered_Data.py
+++ Week_07_US_Govt_Spending/MP_Dash_Callback_w_Filtered_Data.py
@@ -1,42 +1,26 @@
-from dash import Dash, dcc, html, Input, Output, jupyter_dash
+from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import dash_ag_grid as dag
 import plotly.express as px
 import polars as pl
-from datetime import datetime
 
-x_vals = [x for x in range(1,11,1)]
-y_vals = [x*x for x in x_vals]
 df = pl.DataFrame(
     {
         'PARITY' :['ODD', 'EVEN']*5,
-        'X' : x_vals,
-        'Y' : y_vals,
+        'X' : [x for x in range(1,11,1)],
+        'Y' : [x*x for x in range(1,11,1)],
     }
 )
 
 grid = dag.AgGrid(
-    rowData=df.to_pandas().to_dict("records"),
+    rowData=[],
     columnDefs=[{"field": i, 'filter': True, 'sortable': True} for i in df.columns],
-    dashGridOptions={"pagination": True}
+    dashGridOptions={"pagination": True},
+    id='my_grid'
 )
 
-#  app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-# app = Dash(external_stylesheets=[dbc.themes.COSMO]
 app = Dash(external_stylesheets=[dbc.themes.SANDSTONE])
-#  app = JupyterDash(__name__)
 
-# app.layout = dbc.Container([
-#     dbc.Row([dcc.Dropdown(['EVEN', 'ODD'],'ODD' , id='demo-dropdown'),]),
-#     html.Div(id='dd-output-container'),
-#     dbc.Row([
-#         dbc.Col(dcc.Graph(id='fig_line')),
-#         dbc.Col(dcc.Graph(id='fig_scatter')),
-#     ]),
-#     dbc.Row([
-#         dbc.Col([grid], id='my_grid'),
-#     ]),
-# ])
 app.layout = dbc.Container([
     dbc.Row([dcc.Dropdown(['EVEN', 'ODD'],'ODD' , id='demo-dropdown'),]),
     html.Div(id='dd-output-container'),
@@ -45,25 +29,20 @@ app.layout = dbc.Container([
         dbc.Col(dcc.Graph(id='fig_scatter')),
     ]),
     dbc.Row([
-        dbc.Col([grid], id='my_grid'),
+        dbc.Col([grid]),
     ]),
 ])
-df_selected = df
+
 @app.callback(
     Output('fig_line', 'figure'),
     Output('fig_scatter', 'figure'),
-    # Output('my_grid', 'grid'),
+    Output('my_grid', 'rowData'),
     Input('demo-dropdown', 'value')
 )
 def update_dashboard(selected_group):
     df_selected = df.filter(pl.col('PARITY') == selected_group)
-    # my_grid = dag.AgGrid(
-    #     rowData=df_selected.to_pandas().to_dict("records"),
-    #     columnDefs=[{"field": i, 'filter': True, 'sortable': True} for i in df.columns],
-    #     dashGridOptions={"pagination": True}
-    # )
     x_ticks = sorted(df_selected['X'].to_list())
-
+    equation = 'Y = X<sup>2</sup>  '
     fig_line = px.line(
         df_selected, 
         x='X', 
@@ -75,7 +54,7 @@ def update_dashboard(selected_group):
         text='Y'
     )
     fig_line.update_layout(
-        title=f'Line with X-Values {selected_group}'.upper(),
+        title=equation + f'Line with X-Values {selected_group}'.upper(),
         xaxis = dict(tickmode = 'array', tickvals = x_ticks),
     )
     fig_line.update_traces(textposition='top center')
@@ -88,13 +67,11 @@ def update_dashboard(selected_group):
         height=400, width=600,
         )
     fig_scatter.update_layout(
-        title=f'Scatter plot with X-Values {selected_group}'.upper(),
+        title=equation + f'Scatter plot with X-Values {selected_group}'.upper(),
         xaxis = dict(tickmode = 'array', tickvals = x_ticks),
     )
     fig_scatter.update_traces(textposition='top center')
-    return fig_line, fig_scatter# , my_grid
+    return fig_line, fig_scatter, df_selected.to_pandas().to_dict('records')
 
-# # Run the app
 if __name__ == '__main__':
-    # app.run_server(debug=True)
-    app.run(jupyter_height=500, jupyter_width="70%")
+    app.run_server(debug=True)
