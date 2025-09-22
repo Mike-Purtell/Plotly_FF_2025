d0ab24eedda7f738f0f8b13a7b3d01476e8fd54a
Michael Purtell <mpurtell1024@gmail.com>
2025-09-21 21:04:03 -0700

    week 38 app works, only expect minor commits from here

--- Week_38_Athlete_Ranks/app.py
+++ Week_38_Athlete_Ranks/app.py
@@ -3,14 +3,21 @@ import plotly.express as px
 import dash
 from dash import Dash, dcc, html, Input, Output
 import dash_mantine_components as dmc
-import dash_ag_grid as dag
+
+pl.Config().set_tbl_cols(10)
+
 dash._dash_renderer._set_react_version('18.2.0')
 
-#  Plot Rank by Year, and Rank by Career Year.
-#  Figure out a good way to filter. Ideas are:
-#       filter by league
-#       Players with 5 or more years
-#       Players who acheived top xx for yy or more years
+'''
+    This dashboard has timeline plots of RANK by year and by calendar year. 
+    added column RANK_MED is the median rank of each athlete
+    added column RANK_MEAN is the mean rank of each athlete
+    athletes within each league are ranked by RANK_MED, and then by RANK_MEAN.
+    The top 5 athletes are included in this dashboard. Could be ehanced for 
+    analysis of lower ranked athletes
+'''
+
+viz_template = 'plotly_dark'
 
 #----- LOAD AND CLEAN THE DATASET
 df_league_names = pl.DataFrame({
@@ -30,7 +37,7 @@ dict_league_abbr_name = dict(zip(
     df_league_names['LEAGUE_ABBR'], df_league_names['LEAGUE_NAME']))
 
 df  = (
-    pl.read_csv('one-hit-wonders.csv')
+    pl.scan_csv('one-hit-wonders.csv')
     .rename(  # upper case all column names, replace spaces with underscores
         lambda c: 
             c.upper()            # column names to upper case
@@ -39,176 +46,164 @@ df  = (
     .filter(~ pl.col('DNP'))
     .select(
         NAME = pl.col('NAME').str.to_titlecase(),
+        FAMILY_NAME = pl.col('NAME').str.to_titlecase().str.split(' ' ).list.last(),
         NAME_COUNT = pl.len().over('NAME').cast(pl.UInt8),
         YEAR = pl.col('YEAR').cast(pl.UInt16),
+        YEARS_TOT = pl.col('YEAR').cast(pl.UInt16).len().over('NAME'),
         CAREER_YEAR = (1 + pl.col('YEAR_INDEX')),
         PEAK_CAREER_YEAR = pl.col('PEAK_YEAR_INDEX'),
-        SPORT = pl.col('SPORT_NAME').str.to_titlecase(),
         TEAM = pl.col('TEAM'),
         PLAYED_VAL = pl.col('PLAYED_VAL').cast(pl.UInt16),
-        RANK = pl.col('RANK').cast(pl.UInt16),
-        HIGHEST_RANK = pl.col('RANK').min().over('NAME').cast(pl.UInt8),
+        RANK = pl.col('RANK'),
+        RANK_MED = pl.col('RANK').cast(pl.UInt16).median().over('NAME', 'LEAGUE').cast(pl.Float32),
+        RANK_MEAN = pl.col('RANK').cast(pl.UInt16).mean().over('NAME').cast(pl.Float32),
         FIRST_YEAR = pl.col('YEAR').min().over('NAME').cast(pl.UInt16),
         LEAGUE_ABBR = pl.col('LEAGUE').str.to_uppercase(),
-    )
-    .join(df_league_names, on = 'LEAGUE_ABBR', how='left')
-    .filter(pl.col('HIGHEST_RANK') <= 10)
-     # .sort('')
-)
-print(df.shape)
-print(df.glimpse())
-
-# #----- FUNCTIONS ---------------------------------------------------------------
-def get_card(title, value, id=''):
-    card_bg_color = '#F5F5F5'
-    return(
-        dmc.Card(children=[
-            dmc.Text(f'{title}', ta='center', fz=24),
-            dmc.Text(f'{value}', ta='center', fz=20, c='blue', id=id,),
-        ],
-        style={'backgroundColor': card_bg_color}, 
-        #mx is left & right margin, my top & bottom margin
-        withBorder=True, shadow='sm', radius='xl', mx=2, my=2
+        NAME_ORG = (  # form of NAME_ORG is NBA: Stephen Curry,  etc.
+            pl.col('LEAGUE').str.to_uppercase() + pl.lit(': ') +
+            pl.col('NAME').str.to_titlecase()
         )
     )
+    .filter(pl.col('YEARS_TOT') >= 5)
+    .sort('LEAGUE_ABBR', 'RANK_MED', 'RANK_MEAN')
+    .collect()
+    .join(df_league_names, on='LEAGUE_ABBR', how='left')
+)
+
+top_5_list = sorted(list(
+    df
+    .lazy()
+    .unique('NAME_ORG')    
+    .sort('LEAGUE_ABBR', 'RANK_MED', 'RANK_MEAN')
+    .with_columns(
+        RANK_LEAGUE = 
+            pl.col('LEAGUE_ABBR')
+            .cum_count()
+            .over('LEAGUE_ABBR')
+            .cast(pl.UInt8)
+        )
+    .filter(pl.col('RANK_LEAGUE') <= 5)
+    .collect()
+    ['NAME_ORG']
+))
+
+#----- FUNCTIONS ---------------------------------------------------------------
+def get_tl_by_year(df_callback, name_org_list):
+    # timeline by calendar year
+    fig = px.line(
+        df_callback.sort('YEAR'),
+        x='YEAR',
+        y='RANK',
+        color='NAME_ORG',
+        template=viz_template,
+        markers=True,
+        line_shape='spline',
+        category_orders={'NAME_ORG': name_org_list}, # handy way to sort legend
+        log_y=True,
+        title='Timeline by calendar year',
+    )
+    fig.update_layout(
+        xaxis_title='CALENDAR YEAR', yaxis_title='RANK -- LOG SCALE',
+        legend_title = 'Athlete'
+        )
+    # high rankings gets a low rank numbers. Reverse y to put #1 at the top
+    fig.update_yaxes(autorange='reversed')
+    return fig
+
+def get_tl_by_career_year(df_callback, name_org_list):
+    # timeline by career year
+    fig = px.line(
+        df_callback.sort('YEAR'),
+        x='CAREER_YEAR',
+        y='RANK',
+        color='NAME_ORG',
+        template=viz_template,
+        markers=True,
+        line_shape='spline',
+        category_orders={'NAME_ORG': name_org_list}, # handy way to sort legend
+        log_y=True,
+        title='Timeline by career year'
+    )
+    fig.update_layout(
+        xaxis_title='CAREER YEAR', yaxis_title='RANK -- LOG SCALE',
+        legend_title = 'Athlete'
+        )
+    # high rankings gets a low rank numbers. Reverse y to put #1 at the top
+    fig.update_yaxes(autorange='reversed')
+    return fig
 
 #----- GLOBALS -----------------------------------------------------------------
 style_horizontal_thick_line = {'border': 'none', 'height': '4px', 
     'background': 'linear-gradient(to right, #007bff, #ff7b00)', 
     'margin': '10px,', 'fontsize': 32}
 
-style_horizontal_thin_line = {'border': 'none', 'height': '2px', 
-    'background': 'linear-gradient(to right, #007bff, #ff7b00)', 
-    'margin': '10px,', 'fontsize': 12}
-
 style_h2 = {'text-align': 'center', 'font-size': '40px', 
             'fontFamily': 'Arial','font-weight': 'bold', 'color': 'gray'}
 
-viz_template = 'plotly_dark'
-
-borough_list = ['Brooklyn', 'Manhattan', 'Queens']
-borough_color_map = {
-    'Brooklyn'    :  'red',     # 'CornflowerBlue',
-    'Manhattan'   :  'navy',    # 'crimson',
-    'Queens'      :  'green',   # 'Chartreuse',
-}
-
-size_color_map = {
-    'Small'        :  '#00FFFF',  
-    'Medium'       :  '#FF007F',  
-    'Large'        :  '#00FF00',  
-    'Extra Large'  :  '#FFD700', 
-}
-league_list = sorted(list(set(df['LEAGUE_ABBR'])))
-print(f'{league_list = }')
-
 #----- DASH COMPONENTS------ ---------------------------------------------------
-
 data = [[i,v] for i, v in dict_league_abbr_name.items()]
-print(f'{data = }')
-# dmc_select_league = dmc.RadioGroup(
-#     children=dmc.Group([dmc.Radio(key, value=val) for key, val in data], my=10),
-#     id='select-league',
-#     value=df_league_names.item(0, 'LEAGUE_NAME'),
-#     label='Select a League',
-#     size="sm",
-#     mb=10,
-# )
-
 dmc_select_league = dmc.Box([
         dmc.Group(
             dmc.ChipGroup(
                 [dmc.Chip(k, value=v) for k,v in dict_league_abbr_name.items()],
-                #     # dmc.Chip("Multiple chips", value="a"),
-                #     # dmc.Chip("Can be selected", value="b"),
-                #     # dmc.Chip("At a time", value="c"),
-                # ],
                 multiple=True,
                 deselectable=True,
                 value=[df_league_names.item(0, 'LEAGUE_NAME')],
-                id='select-league',
+                id='select-leagues',
             ),
             justify='left',
         ),
-        dmc.Text(id='selected-league', ta='center')
     ])
 
 dmc_selected_leagues = dmc.GridCol(
     dmc.GridCol(
         dmc.Text(
-            id='selected_league',
+            id='selected_leagues',
             style={'fontSize': '18px','color': 'blue'}
-            ), 
+        ), 
     )
 )
-
-
-# dmc.RadioGroup(
-#     children=dmc.Group([dmc.Radio(key, value=val) for key, val in data], my=10),
-#     id='select-league',
-#     value=df_league_names.item(0, 'LEAGUE_NAME'),
-#     label='Select a League',
-#     size="sm",
-#     mb=10,
-# )
-
-
-
-#----- INFO CARDS --------------------------------------------------------------
-
-
 #----- DASH APPLICATION STRUCTURE---------------------------------------------
 app = Dash()
 server = app.server
 app.layout =  dmc.MantineProvider([
     html.Hr(style=style_horizontal_thick_line),
-    dmc.Text('Athletic Longevity', ta='center', style=style_h2),
+    dmc.Text('Consistency - top 5 athletes per league', ta='center', style=style_h2),
     html.Hr(style=style_horizontal_thick_line),
+    dmc.Grid(children = [dmc.GridCol(dmc_select_league, span=8, offset = 1)]),
+    dmc.Grid(children = [dmc.GridCol(dmc_selected_leagues, span=8, offset = 1)]),
     dmc.Grid(children = [
-        dmc.GridCol(dmc_select_league, span=8, offset = 1),
-    ]),
-    dmc.Grid(children = [
-        dmc.GridCol(dmc.Text('Selected Leagues: '), ta='left', span=8, offset = 1)
-    ]),
-    dmc.Grid(children = [ 
-        dmc.GridCol(dmc_selected_leagues, span=8, offset = 1),
-    ]),
-    #     dmc.GridCol(
-    #         dmc.Text(
-    #             id='selected_league',
-    #             style={
-    #                 'fontSize': '18px',
-    #                 'color': 'blue'
-    #                 }
-    #             ), 
-    #         span = 8, offset=1
-    #     )
-    # ]),
-
-    dmc.Grid(children = [
-        dmc.GridCol(
-            dmc.Text(
-                id='selected_league',
-                style={
-                    'fontSize': '18px',
-                    'color': 'blue'
-                    }
-                ), 
-            span = 8, offset=1
-        )
+        dmc.GridCol(dcc.Graph(id='by-year'), span=6, offset=0),            
+        dmc.GridCol(dcc.Graph(id='by-career-year'), span=6, offset=0), 
     ]),
 ])
 
 @app.callback(
-    Output('selected_league', 'children'), 
-    Input('select-league', 'value')
+    Output('selected_leagues', 'children'), 
+    Output('by-year', 'figure'), 
+    Output('by-career-year', 'figure'), 
+    Input('select-leagues', 'value')
 )
 def choose_framework(value):
     print(f'{value = }')
-    return  ', '.join(sorted(value))
-
-# f'Selected Leagues: .join(sorted(value))'
-
-
+    df_callback=(
+        df
+        .filter(pl.col('LEAGUE_NAME').is_in(value))
+        .filter(pl.col('NAME_ORG').is_in(top_5_list))
+    )
+    name_org_list = (
+        df_callback
+        .unique('NAME_ORG')
+        .sort('LEAGUE_NAME', 'FAMILY_NAME')
+        ['NAME_ORG']
+        .to_list()
+    )
+    tl_by_year= get_tl_by_year(df_callback, name_org_list)
+    tl_by_career_year= get_tl_by_career_year(df_callback, name_org_list)
+    return  (
+        ', '.join(sorted(value)),
+        tl_by_year,
+        tl_by_career_year
+    )
 if __name__ == '__main__':
     app.run(debug=True)
